2d4f0410a163c17e4f0a7c81850fc9d648594c35
Aisbondeng <Kpphmri@gmail.com>
2025-05-31 04:00:34 +0800

    fix: correct import syntax in HomePage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,4 +1,5 @@
 import React
+import { useState } from 'react';
 import { useWalletConnection } from "../hooks/useWalletConnection";, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { Input } from '@/components/ui/input';
